644adc5cb79348463138a29efef40861713cc76e
celine28240 <celinehenichard@gmail.com>
2019-04-25 16:47:49 +0200

    création des pages contact-aboutus-legalinformation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LegalInformationComponent } from './legal-information/legal-information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    AboutUsComponent,
+    LegalInformationComponent
   ],
   imports: [
     BrowserModule,
